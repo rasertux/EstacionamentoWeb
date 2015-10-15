ccf8ad0a838e75ea74911e49887cd9366a8acf6a
rasertux <rafaelsergiocosta@gmail.com>
2015-10-15 15:24:23 -0300

    Adicionado script SQL-DDL

---
+++ Scripts SQL/Estacionamento-DDL.sql
@@ -0,0 +1,20 @@
+create database estacionamento;
+
+use estacionamento;
+
+create table tarifa(
+	idtarifa int(8) auto_increment not null,
+	descricao varchar(30) not null,
+	valor decimal(15,2) not null,
+	primary key(idtarifa)
+);
+
+create table veiculo(
+	placa char(7) not null,
+	marca varchar(20) not null,
+    modelo varchar(20) not null,
+    idtarifa int(8) not null,
+    primary key(placa),
+    foreign key(idtarifa)
+    references tarifa(idtarifa)
+);
